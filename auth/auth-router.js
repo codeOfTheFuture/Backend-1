4a598d2ec9693bb622f247260a0031a47729f274
Jeff Oliver <jeffoliver3384@gmail.com>
2019-08-24 15:16:44 -0400

    created endpoint for user to login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const User = require('../users/users-model');
+const Users = require('../users/users-model');
 const secrets = require('../config/jwtSecret');
+
+// Load user model
+const User = mongoose.model('User');
 
 // Register a new user
 router.post('/register', (req, res) => {
@@ -13,7 +17,7 @@ router.post('/register', (req, res) => {
 
   user.password = hashPassword;
 
-  const newUser = new User({
+  const newUser = new Users({
     username: user.username,
     password: user.password,
   });
@@ -30,6 +34,28 @@ router.post('/register', (req, res) => {
       res.status(401).json({ message: `Please enter a username and password` });
     }
   });
+});
+
+// Login a user
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username })
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          user: { id: user.id, username: user.username },
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 // Generate JSON Web Token
--- users/users-router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// User Model
-const User = require('');
-
-// @route POST api/users/register
